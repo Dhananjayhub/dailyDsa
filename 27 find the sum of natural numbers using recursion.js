76e19078176af19cebc6af8adecfa2a008b171bf
dhananhub <dkjha444@gmail.com>
2023-10-16 23:30:35 +0530

    add20

--- 27 find the sum of natural numbers using recursion.js
+++ 27 find the sum of natural numbers using recursion.js
@@ -1,22 +1,43 @@
-const prompt = require("prompt-sync") ();
+// const prompt = require("prompt-sync") ();
 
-const num = prompt('Enter a first integer: ');
-
-
+// const num = prompt('Enter a first integer: ');
 
 
-let a = prompt("enter no ");
-let total = 0;
-let j = 1;
-function sum(a){
-    if(j>a){ 
-        return total;
-    }
-    total+=j;
-    j++;
-    console.log(j);
-    console.log(total);
-    return sum(a);
-}
-let b = sum(a)
-console.log(b);
+
+
+// let a = prompt("enter no ");
+// let total = 0;
+// let j = 1;
+// function sum(a){
+//     if(j>a){ 
+//         return total;
+//     }
+//     total+=j;
+//     j++;
+//     console.log(j);
+//     console.log(total);
+//     return sum(a);
+// }
+// let b = sum(a)
+// console.log(b);
+
+let arr= [5,4,9,7,3,7]
+let ans =[];
+        let temp=0
+        
+        for(let i=0; i<arr.length; i++){
+            if(arr[i]>arr[i+1]) {
+               temp= arr[i+1]
+                arr[i+1]=arr[i];
+                arr[i]= temp;
+                
+               
+                
+            }
+            console.log(arr[i]);
+            
+                }
+            
+            
+        
+        
